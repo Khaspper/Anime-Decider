db09cd7a5b950363cc196ee4a57b07409edcdc69
Mark <brandonnarciso24@gmail.com>
2022-08-20 18:25:53 -0700

    Adding genres

---
+++ Decision-making.py
@@ -0,0 +1,8 @@
+from random import choice
+
+anime = [["One punchman", "Naruto", "Naruto Shippuden", "The Rising of the Sheild Hero", "Astro Boy",
+         "Hunter x Hunter", "Dororo", "Devilman crybaby", "Darling In The Franxx",  "Black Clover", 
+         "Jujutsu Kaisen", "Fullmetal Alchemist"],
+        ["Clannad", "Toradora", "Your lie in april", "Love is war", "Highschool of the dead", "Silent Voice"]]
+
+genre = input("What type of genre are you looking for? ")
